c1fb1e8e0a8d659614237cbbd70fb35d5f96f425
Christian Fenn <cfenn2020@fau.edu>
2022-12-17 13:12:06 -0500

    Exercise 1.2 updated

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,10 +1,10 @@
 const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-  )
+  console.log(props)
+  return <h1>{props.course}</h1>
 }
 
 const Part = (props) => {
+  console.log(props)
   return (
     <p>
         {props.part} {props.exercises}
@@ -12,12 +12,12 @@ const Part = (props) => {
   )
 }
 
-const Content = () => {
+const Content = (props) => {
   return (
     <div>
-        <Part part='Fundamentals of React' exercises='10' />
-        <Part part='Using props to pass data' exercises='7' />
-        <Part part='Fundamentals of React' exercises='14' />
+        <Part part={props.part1} exercises={props.exercises1} />
+        <Part part={props.part2} exercises={props.exercises2} />
+        <Part part={props.part3} exercises={props.exercises3} />
     </div>
   )
 }
@@ -32,14 +32,17 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
   const exercises1 = 10
+  const part2 = 'Using props to pass data'
   const exercises2 = 7
+  const part3 = 'State of a component'
   const exercises3 = 14
 
   return (
     <div>
       <Header course={course} />
-      <Content />
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
