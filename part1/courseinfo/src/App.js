6f255499ddc32d09c985b8729727a7dededf9663
Christian Fenn <cfenn2020@fau.edu>
2022-12-17 12:19:41 -0500

    Exercise 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -4,11 +4,21 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <p>
         {props.part} {props.exercises}
     </p>
+  )
+}
+
+const Content = () => {
+  return (
+    <div>
+        <Part part='Fundamentals of React' exercises='10' />
+        <Part part='Using props to pass data' exercises='7' />
+        <Part part='Fundamentals of React' exercises='14' />
+    </div>
   )
 }
 
@@ -22,19 +32,14 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
   const exercises1 = 10
-  const part2 = 'Using props to pass data'
   const exercises2 = 7
-  const part3 = 'State of a component'
   const exercises3 = 14
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
