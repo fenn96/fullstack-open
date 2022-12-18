1ff5aa89122f3d32de1dc97200a12a3414cd8e86
Christian Fenn <cfenn2020@fau.edu>
2022-12-18 11:32:03 -0500

    Exercise 1.6 redo

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -12,21 +12,45 @@ const Button = (props) => (
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [stats, setClicks] = useState({
+    good: 0, neutral: 0, bad: 0
+  })
+
+  const handleGoodClick = () => {
+    const newStats = { 
+      ...stats,
+      good: stats.good + 1
+    }
+    setClicks(newStats)
+  }
+
+  const handleNeutralClick = () => {
+    const newStats = { 
+      ...stats, 
+      neutral: stats.neutral + 1
+    }
+    setClicks(newStats)
+  }
+
+  const handleBadClick = () => {
+    const newStats = { 
+      ...stats,
+      bad: stats.bad + 1
+    }
+    setClicks(newStats)
+  }
 
   return (
     <div>
       <Header name='give feedback' />
-      <Button handleClick={() => setGood(good + 1)} text="good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Button handleClick={handleGoodClick} text="good" />
+      <Button handleClick={handleNeutralClick} text="neutral" />
+      <Button handleClick={handleBadClick} text="bad" />
 
       <Header name='statistics' />
-      <Display name='good' value={good} />
-      <Display name='neutral' value={neutral} />
-      <Display name='bad' value={bad} />
+      <Display name='good' value={stats.good} />
+      <Display name='neutral' value={stats.neutral} />
+      <Display name='bad' value={stats.bad} />
     </div>
   )
 }
