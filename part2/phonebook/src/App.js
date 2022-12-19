170490bd5cfb538870ae6b5bbbd565fd8b9232f3
Christian Fenn <cfenn2020@fau.edu>
2022-12-19 11:22:07 -0500

    Exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,29 +1,31 @@
 import { useState } from 'react'
 
-const Person = ({ name }) => {
+const Person = ({ person }) => {
   return (
-    <li>{name}</li>
+    <li>{person.name} {person.number}</li>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '727-555-5555' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const addName = (event) => {
+  const addPerson = (event) => {
     event.preventDefault()
     const result = persons.filter(person => person.name === newName).length > 0 ? false : true;
 
     if (result) {
-      const nameObject = { name: newName }
+      const nameObject = { name: newName, number: newNumber }
       setPersons(persons.concat(nameObject))
     } else {
       window.alert(`${newName} is already added to phonebook`)
     }
     
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
@@ -31,13 +33,17 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
+      <form onSubmit={addPerson}>
+        <div>name: <input value={newName} onChange={handleNameChange} /></div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
         <div>
           <button type="submit">add</button>
         </div>
@@ -45,7 +51,7 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {persons.map(person =>
-          <Person key={person.name} name={person.name} />
+          <Person key={person.name} person={person} />
           )}
       </ul>
     </div>
